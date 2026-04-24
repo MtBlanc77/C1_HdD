423e228b910192158b173847b62e568259015cf3
MtBlanc77 <nsalberto55@gmail.com>
2026-04-24 15:11:48 -0400

    base proyecto

---
+++ src/main/java/cl/usm/certamen1_hdd/Certamen1HdDApplication.java
@@ -0,0 +1,13 @@
+package cl.usm.certamen1_hdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Certamen1HdDApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Certamen1HdDApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cl/usm/certamen1_hdd/Certamen1HdDApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.usm.certamen1_hdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Certamen1HdDApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
